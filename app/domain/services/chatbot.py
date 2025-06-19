04a540039eaf4092c2b93218aba402ec2fd33530
Edward Gonzalez <backendev.py@gmail.com>
2025-06-19 17:19:21 -0500

    Add Aplication Routes & Readme document

--- app/domain/services/chatbot.py
+++ app/domain/services/chatbot.py
@@ -1,14 +1,17 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from config import settings
-from fastapi import HTTPException
 
 
 async def get_chatbot_response(question: str, role: str) -> str:
     chat_model = ChatOpenAI(model=settings.OPENAI_MODEL, openai_api_key=settings.OPENAI_API_KEY)
     prompt_template = PromptTemplate(
         input_variables=["role", "question"],
-        template="Eres un {role}. Responde a la siguiente pregunta: {question}"
+        template=f"""
+        Eres un {role}. Debes proporcionar respuestas detalladas y precisas basadas en tu experiencia y conocimiento en este campo,
+        para responder la siguiente pregunta:  "{question}".
+        Responde de manera clara y profesional, asegurándote de abordar todos los aspectos relevantes del tema.
+        """
     )
     chain = prompt_template | chat_model
     response = chain.invoke({"role": role, "question": question})
